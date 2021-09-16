a2b30cee0ab4f66bf10c2d7862f7663a9ded02b0
Will-Hutchings <88373602+Will-Hutchings@users.noreply.github.com>
2021-09-16 14:15:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+import fetch from 'node-fetch';
+
 !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="yAalLg2aJXJrvLzNyAvV1mFL79n6OfaP";analytics.SNIPPET_VERSION="4.13.2";
   analytics.load("yAalLg2aJXJrvLzNyAvV1mFL79n6OfaP");
   analytics.reset();
   
   }}();
 
- 	  
+ 
+  function optout(){
+	var url = "https://rest.iad-06.braze.com/email/status";
+
+	const body = {
+ 	   "email": "007@caci.co.uk",
+	"subscription_state": "subscribed"
+	};
+
+	const response = await fetch(url, {
+		method: 'POST',
+    	headers: {'Content-Type': 'application/json',
+    	'Authorization': 'Bearer e38c7992-664b-4df6-9b25-81ac5748959d'},
+		body: JSON.stringify(body)
+	
+	});	  
+  }
   
   function sendTrack(){
 	  var uid = document.getElementById("UID").value;
